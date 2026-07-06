bc2ef7e4b75590bc7ca00b333cda7aa6a8c05509
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-06 20:34:18 +0530

    Add files via upload

---
+++ DataStructure/Array/a1.py
@@ -0,0 +1,12 @@
+marks = [88,72,99,80,10]
+
+print(marks[1:4])
+print(marks[::2])
+
+# add to end O(1)
+# marks.append(100)
+# print(marks)
+
+# #Specific Position O(n)
+# marks.append(2,55)
+# print(marks)

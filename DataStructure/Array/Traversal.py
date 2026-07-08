2af55739bc0fef06df6055514bd31c6b4c5177e6
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-08 18:51:56 +0530

    Add files via upload

---
+++ DataStructure/Array/Traversal.py
@@ -0,0 +1,15 @@
+marks = [88,90,99,73,22]
+
+# print(marks[0:])
+# print(marks[:-1])
+
+# print(len(marks))
+
+# for mark in marks:
+#     print(mark)
+
+# for i in range(len(marks)):
+#     print(marks[i])
+
+for mark in reversed(marks):
+    print(mark)

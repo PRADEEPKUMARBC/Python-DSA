ab58145eb123468e9cf4a135f716f1ec90342e79
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-13 10:45:06 +0530

    Add files via upload

---
+++ Hashing/coll.py
@@ -0,0 +1,14 @@
+users = {}
+
+users["pradeepkumarbc138@gmail.com"] = "Pradeep Kumar"
+users["pradeepkumarbc716@gmail.com"] = "Pradeep"
+users["pbc@gmail.com"] = "Pradeep Kumar B C"
+
+# key = "pradeepkumarbc138@gmail.com"
+# value = "Pradeep Kumar"
+# box = hash(key) % total_boxes
+# store(box, "Ravi Sharma")
+
+print(hash("pradeepkumarbc138@gmail.com"))
+print(hash("pradeepkumarbc716@gmail.com"))
+print(hash("pbc@gmail.com"))

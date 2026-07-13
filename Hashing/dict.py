12f782dbea3ea356b58d5dbbe4012c264760ff0e
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-13 10:46:24 +0530

    Add files via upload

---
+++ Hashing/dict.py
@@ -0,0 +1,10 @@
+users = {}
+
+# insert
+users["pradeepkumarbc138@gmail.com"] = "Pradeep Kumar"
+users["pradeepkumarbc716@gmail.com"] = "Pradeep"
+users["pbc@gmail.com"] = "Pradeep Kumar B C"
+
+print(users["pbc@gmail.com"])
+
+print("pradeepkumarbc138@gmail.com" in users)

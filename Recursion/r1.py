4fbda8abfdb1398b57d6df24fc63121e067acfdf
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-14 09:29:16 +0530

    Add files via upload

---
+++ Recursion/r1.py
@@ -0,0 +1,7 @@
+def eat_mangoes(count):
+    if count == 0:
+        print("Hand is empty. Done")
+        return
+    print(f"I have {count} mangoes. eating one")
+    eat_mangoes(count - 1)
+eat_mangoes(5)

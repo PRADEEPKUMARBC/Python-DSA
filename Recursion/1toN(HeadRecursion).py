67cd19644a5cf3f41a5c85c197c09734f083ddb9
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-19 23:52:26 +0530

    Add files via upload

---
+++ Recursion/1toN(HeadRecursion).py
@@ -0,0 +1,30 @@
+def func(i,n):
+    if i > n:
+        return
+    print(i)
+    func(i+1,n)
+
+def func(i,n):
+    if i > n:
+        return
+    print(i)
+    func(i+1,n)
+
+def func(i,n):
+    if i > n:
+        return
+    print(i)
+    func(i+1,n)
+
+def func(i,n):
+    if i > n:
+        return
+    print(i)
+    func(i+1,n)
+
+def func(i,n):
+    if i > n:
+        return
+    print(i)
+    func(i+1,n)
+func(1,5)

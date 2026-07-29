410e44a6caad8ab891367524e25a6106d0ca8273
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-07-29 14:02:11 +0530

    Add files via upload

---
+++ BinarySearch/IntroductiontoBinarySearch.py
@@ -0,0 +1,17 @@
+nums = [2,4,6,7,9,11,18,19]
+
+def binarySearch(nums, target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binarySearch(nums, 19))

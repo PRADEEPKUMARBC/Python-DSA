b44374053e44628d78583279fd1b387e093dcbd2
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-08-28 00:03:39 +0530

    Add files via upload

---
+++ LargestCommonPrefix.py
@@ -0,0 +1,13 @@
+strs = ["flower", "flow", "flowers"]
+def largestCommonPrefix(strs):
+    if len(strs) == 0:
+        return ""
+    result = ""
+    base = strs[0]
+    for i in range(0, len(base)):
+        for word in strs[1:]:
+            if i == len(word) or word[i] != base[i]:
+                return result
+        result += base[i]
+    return result
+print(largestCommonPrefix(strs))

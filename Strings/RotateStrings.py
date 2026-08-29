6a71e770dafddf8359564bb66a382f7d22d8d03e
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-08-29 12:25:12 +0530

    Add files via upload

---
+++ Strings/RotateStrings.py
@@ -0,0 +1,13 @@
+s = "tree"
+
+def SortCharacterFreq(s):
+    result = ""
+    hash_map = {}
+    for ch in s:
+        hash_map[ch] = hash_map.get(ch, 0) + 1
+    
+    sorted_chars = sorted(hash_map.items(), key=lambda x:x[1],reverse=True)
+    for ch, freq in sorted_chars:
+        result = result + (ch * freq)
+    return result
+print(SortCharacterFreq(s))
